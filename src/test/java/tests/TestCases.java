8a0910a17c9ca158c2136e3ebd2bb55ee243ab3a
Tims <92653720+timsBt@users.noreply.github.com>
2023-04-25 09:28:15 +0400

    Update TestCases.java

--- src/test/java/tests/TestCases.java
+++ src/test/java/tests/TestCases.java
@@ -2,8 +2,8 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomeLogin;
 import pages.MainPage;
